5e2d5d204db473d8d6e41dedc9a92319d251b6b2
pakky94 <m.pacchialat@gmail.com>
2020-03-18 11:40:53 +0100

    added docs folder

---
+++ docs/precache-manifest.4642b3063d363a412f78b4a64495fc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be55e18799b2d77aa69b78bce91e8c46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa57f177213c356257c2",
+    "url": "/static/css/main.6e443adf.chunk.css"
+  },
+  {
+    "revision": "c66264a3790ee01ecadf",
+    "url": "/static/js/2.78ed0734.chunk.js"
+  },
+  {
+    "revision": "41679afd1a1eaf7f78708343ebb0cba7",
+    "url": "/static/js/2.78ed0734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa57f177213c356257c2",
+    "url": "/static/js/main.26abbf8f.chunk.js"
+  },
+  {
+    "revision": "c27f76a36f8d51c29fd4",
+    "url": "/static/js/runtime-main.7bc453c4.js"
+  }
+]);
